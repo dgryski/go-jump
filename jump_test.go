2a32a091ac42cb4220ffb377a7029e9df51953bf
Damian Gryski <damian@gryski.com>
2014-08-06 17:21:10 +0200

    include tests from Guava
    Turns out Jump Hash was open-sourced into Guava at the end of 2011 with
    no announcement.  The comments indicate it was ported to Java from their
    internal C++ code.
    
    These tests come from
    https://code.google.com/p/guava-libraries/source/browse/guava-tests/test/com/google/common/hash/HashingTest.java

--- jump_test.go
+++ jump_test.go
@@ -26,3 +26,35 @@ func TestHash(t *testing.T) {
 	}
 
 }
+
+// From Guava
+func TestGolden(t *testing.T) {
+
+	golden100 := []int32{0, 55, 62, 8, 45, 59, 86, 97, 82, 59, 73, 37, 17, 56, 86, 21, 90, 37, 38, 83}
+	for i, v := range golden100 {
+		if g := Hash(uint64(i), 100); g != v {
+			t.Errorf("golden100 failed: Hash(%v, 100)=%v, want %v\n", i, g, v)
+
+		}
+	}
+
+	var tests = []struct {
+		k       uint64
+		buckets int
+		out     int32
+	}{
+		{10863919174838991, 11, 6},
+		{2016238256797177309, 11, 3},
+		{1673758223894951030, 11, 5},
+		{2, 100001, 80343},
+		{2201, 100001, 22152},
+		{2202, 100001, 15018},
+	}
+
+	for _, tt := range tests {
+		if g := Hash(tt.k, tt.buckets); g != tt.out {
+			t.Errorf("compat failed: Hash(%v, %v)=%v, want %v\n", tt.k, tt.buckets, g, tt.out)
+
+		}
+	}
+}
